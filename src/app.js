3b97bb640a42154fa954f6f1bfa1a33cf239f079
Gileandro Campos <gileandrocampos8@gmail.com>
2024-06-22 16:46:37 -0400

    feat: create method to create Posts && render in DOM

--- src/app.js
+++ src/app.js
@@ -1,50 +1,16 @@
-import { Data } from "./date.js";
-import { Element } from "./element.js";
-import { iconHeart } from "./iconHeart.js";
 import { Like } from "./like.js";
 import { Sanitize } from "./sanitize.js";
 import { Notice } from "./service/notice.js";
+import { View } from "./view.js";
 
 const BASE_URL = 'https://servicodados.ibge.gov.br/api/v3/noticias/?busca=tecnologia'
 const listPost = document.querySelector('[data-js="list-post"]')
 const inputSearch = document.querySelector('[data-js="input-search"]')
 
-const createPosts = async (arrItems) => {
+const render = async (arrItems) => {
   listPost.innerHTML = ""
-  const fragment = new DocumentFragment()
-  const posts = arrItems.map(async element => {
-    const container = Element.createElement("div");
-    Element.addClass(container, "post");
-
-    const containerInfo = Element.createElement("div");
-    Element.addClass(containerInfo, "post__info");
-
-    const containerContent = Element.createElement("div");
-    Element.addClass(containerContent, "post__content");
-
-    const title = Element.createElement("h2");
-    Element.addClass(title, "post__title");
-    Element.insertContent(title, element.titulo);
-
-    const description = Element.createElement("p");
-    Element.addClass(description, "post__description");
-    Element.insertContent(description, element.introducao);
-
-    const date = Element.createElement("span");
-    Element.addClass(date, "post__date");
-    Element.insertContent(date, Data.newDate(element.data_publicacao));
-
-    containerInfo.append(date);
-    containerInfo.append(await iconHeart());
-    containerContent.append(title);
-    containerContent.append(description);
-    container.append(containerInfo);
-    container.append(containerContent);
-    fragment.appendChild(container);
-  });
-
-  await Promise.all(posts);
-  listPost.append(fragment);
+  const posts = await View.renderPosts(arrItems)
+  listPost.append(posts);
 
   const iconLike = listPost.querySelectorAll(".post__icon")
   iconLike.forEach(like => {
@@ -61,7 +27,7 @@ const app = async () => {
   try {
     const notices = await Notice.getNotices(BASE_URL)
     if (notices && notices.items) {
-      createPosts(notices.items)
+      render(notices.items)
     } else {
       console.error("Invalid API response:", notices)
     }
@@ -86,11 +52,11 @@ inputSearch.addEventListener('input', debounce(async (e) => {
     const value = Sanitize.clear(e.target.value);
     if (value) {
       const search = await Notice.searchNotices(BASE_URL, value);
-      createPosts(search);
+      render(search);
     } else {
       const notices = await Notice.getNotices(BASE_URL);
       if (notices && notices.items) {
-        createPosts(notices.items);
+        render(notices.items);
       } else {
         console.error('Invalid API response:', notices);
       }
---
+++ src/view.js
@@ -0,0 +1,36 @@
+import { Data } from "./date.js";
+import { Element } from "./element.js";
+import { iconHeart } from "./iconHeart.js";
+
+export class View {
+  static async renderPosts(arrItems) {
+    const fragment = new DocumentFragment()
+    const posts = arrItems.map(async element => {
+      const postContent = await this.createPost(element)
+      fragment.appendChild(postContent);
+    });
+
+    await Promise.all(posts)
+    return fragment
+  }
+
+  static async createPost(element) {
+    const container = Element.create("div", "post");
+    const containerInfo = Element.create("div", "post__info");
+    const containerContent = Element.create("div", "post__content");
+
+    const title = Element.create("h2", "post__title", element.titulo);
+    const description = Element.create("p", "post__description", element.introducao);
+
+    const date = Element.create("span", "post__date", element.data_publicacao);
+
+    containerInfo.append(date);
+    containerInfo.append(await iconHeart());
+    containerContent.append(title);
+    containerContent.append(description);
+    container.append(containerInfo);
+    container.append(containerContent);
+
+    return container
+  }
+}
